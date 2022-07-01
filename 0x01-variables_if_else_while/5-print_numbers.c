22ff649f38d492e1a7abcb79da24c2fc26051696
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-01 16:11:44 -0700

    commiting task  5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,14 @@
 */
 int main(void)
 {
-        char d = '0';
+char d = '0';
 
-        while (d <= '9')
-        {
-        putchar(d);
-        d++;
-        }
-        putchar('\n');
-        return (0);
+while (d <= '9')
+{
+putchar(d);
+d++;
 }
+putchar('\n');
+return (0);
+}
+
