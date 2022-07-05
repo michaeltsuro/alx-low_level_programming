ccd7c93f65c2bced31a412a764c4fae1e6f6dc99
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-05 13:31:38 -0700

    Print the string _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putchar -writes the string _putchar to stdout
+ * @_putchar: The string to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
