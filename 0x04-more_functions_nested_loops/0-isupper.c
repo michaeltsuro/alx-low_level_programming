7891fe84490ee8a1090ccdf3e6ff8486d68a5089
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-07 16:41:52 -0700

    Determin if its an uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - print 1 if its uppercase 0 otherwise
+ * int_isupper - print 1 if its uppercase 0 otherwise
  * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
-	if (c>64 && c<91)
+	if (isupper (c))
 	{
 		return (1);
 	}
