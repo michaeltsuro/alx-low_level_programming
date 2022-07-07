e5b30e9343e33647d246871a1a1c7869e1d83bf9
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-07 16:25:52 -0700

    Determine if a character is in uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - print 1 if its uppercase 0 otherwise
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
