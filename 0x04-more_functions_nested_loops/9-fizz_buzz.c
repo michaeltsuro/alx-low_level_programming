1619a7409dd04e9968d031ab737401088d70264e
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-08 06:44:10 -0700

    Print Random

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,31 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Fizz-Buzz test
+ * Description: print numbers 1 -100 replacing multiples of 3 with Fizz,
+ * multiples of 5 with Buzz, and multiples of both with FizzBuzz
  * Return: 0
  */
+
 int main(void)
+
 {
+
 	int i;
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if ((i%5) == 0)
-		{
-			printf("Buzz");
-		}
-		else if ((i%3) == 0)
-		{
-			printf("Fizz");
-		}
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-		{
-			printf("%d",i);
-		}
+			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
+
 }
