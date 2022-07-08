b9f8dad412a4f595471702312459f3a5b2ad559a
Takudzwa Michael Tsuro <michaeltsuro@gmail.com>
2022-07-07 17:52:11 -0700

    Printlines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print lines
+ * @n: number of lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for(i = 1; i <= n; i++)
+		{
+			putchar(');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
